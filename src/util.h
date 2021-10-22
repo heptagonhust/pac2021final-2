8d1c623628c8a8b4bcbb4727598a8d9ec69d0c1e
ueqri <ueqri@outook.com>
2021-10-22 01:13:06 +0000

    Use std allocator

--- src/util.h
+++ src/util.h
@@ -1,6 +1,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <memory>
 #include <stdlib.h>
 #include <string>
 #include <iostream>
@@ -12,7 +13,7 @@
 #include <boost/thread/mutex.hpp>
 #include "common.h"
 #include <parallel_hashmap/phmap.h>
-#include <boost/pool/pool_alloc.hpp>
+// #include <boost/pool/pool_alloc.hpp>
 
 #define SUBMAP_NUM_EXP2 4
 
@@ -20,7 +21,7 @@ using namespace std;
 using phmap::parallel_flat_hash_map;
 typedef parallel_flat_hash_map<uint64, Position1,  std::hash<uint64>, 
 														phmap::priv::hash_default_eq<uint64>,
-                            boost::fast_pool_allocator<std::pair<const uint64, Position1>>, SUBMAP_NUM_EXP2, phmap::NullMutex> BarcodeMap;
+                            std::allocator<std::pair<const uint64, Position1>>, SUBMAP_NUM_EXP2, phmap::NullMutex> BarcodeMap;
 
 inline bool starts_with(string const& value, string const& starting)
 {
